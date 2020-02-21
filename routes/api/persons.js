00391c93c1996f38a01f1d90be7456aca26de238
poeticinjustice <poeticinjustice@gmail.com>
2020-02-21 01:47:17 -0500

    update persons route

--- routes/api/persons.js
+++ routes/api/persons.js
@@ -124,4 +124,86 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route    POST api/persons/researchPost/:id
+// @desc     Post research on a person
+// @access   Private
+router.post(
+  '/researchpost/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const person = await Person.findById(req.params.id);
+
+      const newResearchPost = {
+        text: req.body.text,
+        link: req.body.link,
+        note: req.body.note,
+        name: user.name,
+        user: req.user.id
+      };
+
+      person.researchPosts.unshift(newResearchPost);
+
+      await person.save();
+
+      res.json(person.researchPosts);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route    DELETE api/persons/researchpost/:id/:researchPost_id
+// @desc     Delete research post
+// @access   Private
+router.delete('/researchpost/:id/:researchpost_id', auth, async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.id);
+
+    // Pull out research post
+    // ERROR: Why is it deleting oldest post rather than post by id??
+    const researchPost = person.researchPosts.find(
+      researchPost => researchPost.id === req.params.researchpost_id
+    );
+
+    // Make sure research post exists
+    if (!researchPost) {
+      return res.status(404).json({ msg: 'Research post does not exist' });
+    }
+
+    // Check user
+    if (researchPost.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIndex = person.researchPosts
+      .map(researchPost => researchPost.id)
+      .indexOf(req.params.researchPost_id);
+
+    person.researchPosts.splice(removeIndex, 1);
+
+    await person.save();
+
+    res.json(person.researchPosts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
